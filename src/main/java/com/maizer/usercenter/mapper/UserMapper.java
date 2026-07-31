7cd2abe95cf53a1ea42aac75dee91b32e960ecef
maizer <ma1zeryu02@gmail.com>
2026-07-31 14:41:07 +0800

    添加远程仓库

---
+++ src/main/java/com/maizer/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.maizer.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.maizer.usercenter.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
--- src/test/java/com/maizer/usercenter/UserCenterApplicationTests.java
+++ src/test/java/com/maizer/usercenter/UserCenterApplicationTests.java
@@ -1,13 +1,30 @@
 package com.maizer.usercenter;
 
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.maizer.usercenter.mapper.UserMapper;
+import com.maizer.usercenter.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class UserCenterApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.isTrue(5 == userList.size(), "");
+        userList.forEach(System.out::println);
     }
 
 }
